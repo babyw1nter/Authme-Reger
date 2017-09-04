2e9cfb932bd2e8fc7e9781fbe89cf87775e420c8
sc0utz <907322015@qq.com>
2017-09-05 03:22:22 +0800

    改写部分function，修复一些前端逻辑问题

--- js/main.js
+++ js/main.js
@@ -1,19 +1,25 @@
 /* ----------------------------------------
  * Index页面控制js(需要jQuery支持)	  
- * 编写: Scoutone 创建日期: 2017.6.14
+ * 编写: sc0utzz 创建日期: 2017.6.14
  * 06-18新增: 邮箱验证码检测
  * 06-19新增: 阿里云盾滑动验证码
+ * 09-03修改: 重写部分function
  * ----------------------------------------
 */
 
-var namecheck = false, pwcheck = false, apwcheck = false, emcheck = false, emkeycheck = false, keycheck = false, afs_ = false;
+var namecheck = false, pwcheck = false, apwcheck = false, emcheck = false, emkeycheck = false, keycheck = true, afs_ = false;
+var isCD = false;
 
 // 设置焦点
 $("#id").focus();
 
+// 禁用邮件发送btn
+$("#sdem").addClass('btn-disabled');
+$("#sdem").attr("disabled", true);
+
 // 检测用户名
 $("#id").focus(function (){
-	$("#id").css("color","#555");
+	$("#id").css("color","#666");
 	$("#i-tt1").hide();
 });
 
@@ -21,108 +27,125 @@ $("#id").blur(function (){
 	var regtext = /^[a-zA-Z][a-zA-Z0-9_]*$/; //正则表达式
 	var userid = $("#id").val(); // 取input内容
 	if (userid.length <= 4 || !regtext.test(userid)) {
-		$("#i-tt1").html("昵称不合法");
-		if(userid != ""){
-			namecheck = false;
-			$("#i-tt1").show();
+		$("#i-tt1").html("<span><i class='ion-span ion-android-alert'></i>5-10个英文+数字+下划线组合</span>");
+		if(userid.length == 0){
+			$("#i-tt1").html("<span><i class='ion-span ion-android-alert'></i>请输入用户名</span>");
 		}
+		namecheck = false;
 	} else {
-		$("#id").css("color","#555");
-		$("#i-tt1").hide();
-		checkun(); // ajax提交检测用户名是否存在
+		$("#id").css("color","#666");
+		//$("#i-tt1").hide();
+		checkun(); // Get提交检测用户名是否存在
+	}
+	$("#i-tt1").show();
+});
+
+// 检测邮箱
+$("#em").focus(function (){
+	$("#em").css("color","#666");
+	$("#i-tt2").hide();
+	if(isCD == false){
+		$("#sdem").addClass('btn-disabled');
+		$("#sdem").attr("disabled", true);
+	}
+});
+
+$("#em").blur(function (){
+	var emtext = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/;
+	var em = $("#em").val();
+	if(isCD == false){
+		// 按钮禁用
+		$("#sdem").addClass('btn-disabled');
+		$("#sdem").attr("disabled", true);
+	}
+	if (em.length <= 6 || !emtext.test(em)) {
+		$("#i-tt2").html("<span><i class='ion-span ion-android-alert'></i>请输入正确的邮箱格式</span>");
+		if(em.length == 0){
+			$("#i-tt2").html("<span><i class='ion-span ion-android-alert'></i>请输入邮箱</span>");
+		}
+		emcheck = false;
+		$("#i-tt2").show();
+	} else {
+		$("#em").css("color","#666");
+		checkem(); // Get提交检测
+	}
+	$("#i-tt2").show();
+});
+
+// 检测邮箱验证码
+$("#emkey").focus(function (){
+	$("#emkey").css("color","#666");
+	$("#i-tt3").hide();
+});
+
+$("#emkey").blur(function (){
+	var emkeytext = /^[0-9]*$/;
+	var emkey = $("#emkey").val();
+	if (emkey.length <= 5 || emkey.length > 6 || !emkeytext.test(emkey)) {
+		$("#i-tt3").html("<span><i class='ion-span ion-android-alert'></i>请输入正确的邮箱验证码格式（4个以上的数字）</span>");
+		if(emkey.length == 0){
+			$("#i-tt3").html("<span><i class='ion-span ion-android-alert'></i>请输入邮箱验证码</span>");
+		}
+		$("#i-tt3").show();
+		emkeycheck = false;
+	} else {
+		$("#emkey").css("color","#666");
+		checkemkey(); // Get提交检测
 	}
 });
 
 // 检测密码
 $("#pw").focus(function (){
-	$("#pw").css("color","#555");
-	$("#i-tt2").hide();
+	$("#pw").css("color","#666");
+	$("#i-tt4").hide();
 });
 
 $("#pw").blur(function (){
 	var pwtext = /^[a-zA-Z][a-zA-Z0-9_]*$/;
 	var pw = $("#pw").val();
 	if (pw.length <= 5 || !pwtext.test(pw)) {
-		if(pw != ""){
-			pwcheck = false;
-			$("#i-tt2").show();
+		$("#i-tt4").html("<span><i class='ion-span ion-android-alert'></i>请输入6-16个（英文，数字，符号）</span>");
+		if(pw.length == 0){
+			$("#i-tt4").html("<span><i class='ion-span ion-android-alert'></i>请输入密码</span>");
 		}
+		pwcheck = false;
+		$("#i-tt4").show();
 	} else {
+		$("#i-tt4").html("<span style='color: #63da5c;'><i class='ion-span ion-ios-checkmark'></i></span>");
 		pwcheck = true;
-		$("#pw").css("color","#555");
-		$("#i-tt2").hide();
+		$("#pw").css("color","#666");
 	}
+	$("#i-tt4").show();
 });
 
 // 检测重复密码
 $("#apw").focus(function (){
-	$("#apw").css("color","#555");
-	$("#i-tt3").hide();
+	$("#apw").css("color","#666");
+	$("#i-tt5").hide();
 });
 
 $("#apw").blur(function (){
 	var pw = $("#pw").val();
 	var apw = $("#apw").val();
-	if (pw != apw) {
-		if(apw != ""){
-			apwcheck = false;
-			$("#i-tt3").show();
+	if (pw != apw || apw.length == 0) {
+		$("#i-tt5").html("<span><i class='ion-span ion-android-alert'></i>两次输入的密码不一致</span>");
+		if(apw.length == 0){
+			$("#i-tt5").html("<span><i class='ion-span ion-android-alert'></i>请输入确认密码</span>");
 		}
+		apwcheck = false;
+		$("#i-tt5").show();
 	} else {
+		$("#i-tt5").html("<span style='color: #63da5c;'><i class='ion-span ion-ios-checkmark'></i></span>");
 		apwcheck = true;
-		$("#apw").css("color","#555");
-		$("#i-tt3").hide();
+		$("#apw").css("color","#666");
 	}
+	$("#i-tt5").show();
 });
 
-// 检测邮箱
-$("#em").focus(function (){
-	$("#em").css("color","#555");
-	$("#i-tt4").hide();
-	$('#em').popover('show');
-});
-
-$("#em").blur(function (){
-	var emtext = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/;
-	var em = $("#em").val();
-	if (em.length <= 6 || !emtext.test(em)) {
-		$('#i-tt4').html('邮箱不正确');
-		if(em != ""){
-			emcheck = false;
-			$("#i-tt4").show();
-		}
-	} else {
-		$("#em").css("color","#555");
-		$("#i-tt4").hide();
-		checkem(); // Ajax提交检测
-	}
-	$('#em').popover('hide');
-});
-
-// 检测邮箱验证码
-$("#emkey").focus(function (){
-	$("#emkey").css("color","#555");
-	$("#i-tt5").hide();
-});
-
-$("#emkey").blur(function (){
-	var emkeytext = /^[0-9]*$/;
-	var emkey = $("#emkey").val();
-	if (emkey.length <= 3 || !emkeytext.test(emkey)) {
-		if(emkey != ""){
-			emkeycheck = false;
-			$("#i-tt5").show();
-		}
-	} else {
-		$("#emkey").css("color","#555");
-		$("#i-tt5").hide();
-		checkemkey(); // Ajax提交检测
-	}
-});
 
 // 检测邀请码
 $("#fkey").focus(function (){
-	$("#fkey").css("color","#555");
+	$("#fkey").css("color","#666");
 	$("#i-tt6").hide();
 	$('#fkey').popover('show');
 });
@@ -137,7 +160,7 @@ $("#fkey").blur(function (){
 		}
 	} else {
 		keycheck = true;
-		$("#fkey").css("color","#555");
+		$("#fkey").css("color","#666");
 		$("#i-tt6").hide();
 	}
 	$('#fkey').popover('hide');
@@ -146,7 +169,7 @@ $("#fkey").blur(function (){
 
 // 判断表单可否提交
 function check(){
-	if(namecheck && pwcheck && apwcheck && emcheck && emkeycheck && keycheck && afs_){
+	if(namecheck && emcheck && emkeycheck && pwcheck && apwcheck && keycheck && afs_){
 		return true;
 	} else {
 		return false;
@@ -161,12 +184,12 @@ function checkun(){
 	var getUrl = "./modules/check.php?action=checkid&username=" + getUn; 
 	$.get(getUrl,function(str){ 
 		if(str == '1'){
-			$('#i-tt1').show();
-			$('#i-tt1').html('该昵称已被注册');
+			$("#i-tt1").html("<span><i class='ion-span ion-android-alert'></i>该用户名已被注册！</span>");
 		} else {
-			$('#i-tt1').hide();
+			$("#i-tt1").html("<span style='color: #63da5c;'><i class='ion-span ion-ios-checkmark'></i></span>");
 			namecheck = true;
 		}
+		$('#i-tt1').show();
 	})
 }
 
@@ -178,12 +201,17 @@ function checkem(){
 	$.get(getUrl,function(str){ 
 		//console.log(str);
 		if(str == '1'){
-			$('#i-tt4').show();
-			$('#i-tt4').html('邮箱已被使用');
+			$("#i-tt2").html("<span><i class='ion-span ion-android-alert'></i>该邮箱已被注册！</span>");
 		} else {
-			$('#i-tt4').hide();
+			$("#i-tt2").html("<span style='color: #63da5c;'><i class='ion-span ion-ios-checkmark'></i></span>");
+			if(isCD == false){
+				// 按钮解禁
+				$("#sdem").removeClass('btn-disabled');
+				$("#sdem").attr("disabled", false);
+			}
 			emcheck = true;
 		}
+		$('#i-tt2').show();
 	})
 }
 
@@ -195,11 +223,12 @@ function checkemkey(){
 	$.get(getUrl,function(str){ 
 		//console.log(str);
 		if(str == '1'){
-			$('#i-tt5').show();
+			$("#i-tt3").html("<span><i class='ion-span ion-android-alert'></i>验证码错误！</span>");
 		} else {
-			$('#i-tt5').hide();
+			$("#i-tt3").html("<span style='color: #63da5c;'><i class='ion-span ion-ios-checkmark'></i></span>");
 			emkeycheck = true;
 		}
+		$('#i-tt3').show();
 	})
 }
 
@@ -207,43 +236,47 @@ function checkemkey(){
 var countdown = 0;  
 function sendem(){
 	if(emcheck){
-		$("#sdem").addClass('disabled');
+		$("#sdem").addClass('btn-disabled');
 		$("#sdem").attr("disabled", true);
 		countdown = 60;
 		settime();
 		var getUrl = "./modules/check.php?action=sendemkey&email=" + $('#em').val();
 		$.get(getUrl,function(str){ console.log(str); });
 		$('#emkey').val("");
+		$("#i-tt3").html("<span style='color: #63da5c;'><i class='ion-span ion-ios-checkmark'></i>验证码已发送至邮箱</span>");
+		$("#i-tt3").show();
 		emkeycheck = false;
 	} 
 }
 
 function settime() {  
     if(countdown == 0) {  
-        $("#sdem").html('发送验证码');
-		$("#sdem").removeClass('disabled');
+        $("#sdem").html('获取验证码');
+		$("#sdem").removeClass('btn-disabled');
 		$("#sdem").attr("disabled", false);
-		$('#i-tt5').css('right','104px');
-        //countdown = 60; 
 		clearTimeout(tmt); 
+		isCD = false;
 		return;
     } else {    
         $("#sdem").html("重新发送(" + countdown + ")");
-		$("#sdem").addClass('disabled');
+		$("#sdem").addClass('btn-disabled');
 		$("#sdem").attr("disabled", true);
+		isCD = true;
         countdown--;  
     }  
+		/*
 		if(countdown < 9){
 			$('#i-tt5').css('right','110px');
 		} else {
 			$('#i-tt5').css('right','116px');
 		}
+		*/
     var tmt = setTimeout(settime, 1000);
 }  
 
 // 创建阿里云盾风控滑动验证条
 var nc = new noCaptcha();
-var nc_appkey = 'FFFF0000000001750EFC';  // 应用标识
+var nc_appkey = 'FFFF0000000001750EFC';  // 应用标识，请勿修改
 var nc_scene = 'register';  // 场景
 var nc_token = [nc_appkey, (new Date()).getTime(), Math.random()].join(':');
 var nc_option = {
@@ -257,17 +290,45 @@ var nc_option = {
 		$('#sig').val(data.sig);
 		$('#token').val(nc_token);
 		$('#scene').val(nc_scene);
+		$("#i-tt6").html("<span style='color: #63da5c;'><i class='ion-span ion-ios-checkmark'></i></span>");
+		$('#i-tt6').show();
 		afs_ = true;
 	}
 };
 nc.init(nc_option);
 
+function ckboxOnClick(){
+	if($("input[type='checkbox']").is(':checked')){
+		$('#rbtn').removeClass('btn-disabled');
+		$("#rbtn").attr("disabled", false);
+		$("#i-tt7").hide();
+	} else {
+		$('#rbtn').addClass('btn-disabled');
+		$("#rbtn").attr("disabled", true);
+		$("#i-tt7").html("<span style='top: 1px; left: 103px;'><i class='ion-span ion-android-alert'></i>瞅啥呢？把这个勾上！</span>");
+		$("#i-tt7").show();
+	}
+}
+
 // 注册按钮点击事件
 function btnclick(){
 	if(check()){
 		$('#rbtn').html('正在注册...');
-		$('#rbtn').addClass('disabled');
+		//$('#rbtn').addClass('btn-disabled');
 		//$("#rbtn").attr("disabled", true);
+	} /* else if(namecheck == false) {
+		$("#i-tt1").show();
+	} else if(emcheck == false) {
+		$("#i-tt2").show();
+	} else if(emkeycheck == false) {
+		$("#i-tt3").show();
+	} else if(pwcheck == false) {
+		$("#i-tt4").show();
+	} else if(apwcheck == false) {
+		$("#i-tt5").show();
+	}  */else if(afs_ == false) {
+		$("#i-tt6").html("<span style='top: 1px; left: 103px;'><i class='ion-span ion-android-alert'></i>请完成滑动验证</span>");
+		$("#i-tt6").show();
 	}
 }
 
