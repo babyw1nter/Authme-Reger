fe1ee92a5f8f947e0ea71b4dbace442a44ff0f9d
sc0utz <907322015@qq.com>
2017-09-05 03:33:08 +0800

    增加一些可定义的配置项

--- modules/config.inc.php
+++ modules/config.inc.php
@@ -1,13 +1,13 @@
 <?php
 
 // 定义网站Url信息, 结尾不带斜杠
-$Web_Url = 'http://www.yourwebsite.com/reg';
+$Web_Url = 'http://your.website.com/Authme-Reger';
 $Web_Url_Msg = 'message.html';
 
 // 定义数据库信息
 $ip = 'localhost';
 $username = 'root';
-$password = '123456';
+$password = '';
 
 // 定义数据库名
 $sqlname = 'mcserver';
@@ -18,7 +18,7 @@ $web_tablename = 'webreg';
 $web_fkey_tablename = 'webreg_fkey';
 
 // 定义注册设置
-$ban_id = '*admin*,*fuck*,*op*'; // 注册保留关键字
+// TODO $ban_id = '*admin*,*fuck*,*op*'; // 注册保留关键字
 $reg_time = 8; // 同IP多少小时内只能注册1个ID
 
 
@@ -31,18 +31,23 @@ $ban_city = '常州';
 // 定义Stmp邮件服务器信息
 $smtp_server = 'smtp.exmail.qq.com';
 $smtp_port = 25;
-$smtp_username_em = 'your@email.com'; // 发件地址
-$smtp_from_username = 'Admin'; // 发件人
-$smtp_username = 'your@email.com';
-$smtp_password = 'password';
-//$smtp_emtitle = '';
+$smtp_username_em = 'admin@qq.com';
+	$smtp_from_username = 'Admin'; // 发件人名称
+$smtp_username = 'admin@qq.com';
+$smtp_password = '123456';
+$smtp_emtitle = '我的世界--邮箱身份验证'; // 邮件标题
+
+// 定义邀请码设置
+$fkey_enabled = false; // true 开启 false 关闭
+$fkey_minlen = 4;
+$fkey_maxlen = 11;
 
 // 定义云盾KEY
 $Access_Key_ID = '';
 $Access_Key_Secret = '';
 
 // 定义authme密码加密算法
-$pw_enc = 'sha256';
+$pw_enc = 'SHA256'; // 现在改成大写了
 $pw_enc_salt_len = 8;
 
 ?>
